3e82cf6a986fa45aa12228aded9bde9a234c874a
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-09-15 15:06:09 +0200

    deleted comments

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,15 +1,6 @@
 function Playlist (props) {
     const playlistUL = props.playlistUL
 
-    // <li><a href="www.spotify.com">gym opera</a></li>
-    // <li><a href="www.spotify.com">Mozart Madness</a></li>
-    // <li><a href="www.spotify.com">dubstep-Opera-Vibes</a></li>
-    // <li><a href="www.spotify.com">road trip opera</a></li>
-    // <li><a href="www.spotify.com">opera hype</a></li>
-    // <li><a href="www.spotify.com">summer 2019 opera tunes</a></li>
-    // <li><a href="www.spotify.com">chilled out opera</a></li>
-    // <li><a href="www.spotify.com">opera underground club mix</a></li>
-
     return (
         <div className="playlist dashed-border-green">
             <h2>Playlist</h2>
